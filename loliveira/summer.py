44e9eefe722003d5c13349311a8a88532765cc41
root <root@localhost.localdomain>
2016-09-05 22:25:56 -0300

    Atividade 1 realizada

---
+++ loliveira/summer.py
@@ -0,0 +1,12 @@
+import sys
+#use this file to write your summer application
+
+#the summer should receive two integers a and b and return:
+#"the sum of a and b is a+b"
+
+a = float(sys.argv[1])
+b = float(sys.argv[2])
+ 
+soma = float(a+b)
+
+print(soma)
